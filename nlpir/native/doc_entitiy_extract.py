91cf2b99e6addfc32d8d8cabd2888b4358b13987
lihea <52914782+lihea@users.noreply.github.com>
2020-11-06 10:53:15 +0800

    Update doc_entitiy_extract.py

--- nlpir/native/doc_entitiy_extract.py
+++ nlpir/native/doc_entitiy_extract.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from nlpir.native.nlpir_base import NLPIRBase
 from ctypes import c_bool, c_char, c_char_p, c_double, c_int, c_uint, POINTER, Structure, byref, c_ulong
-import typing
+import typing  三生三世
 
 
 class DocExtractor(NLPIRBase):
